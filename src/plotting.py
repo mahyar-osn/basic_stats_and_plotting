a611fd06f59dca288691902eb3d8559991adfe6e
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2019-02-18 11:58:04 +1300

    Add method to do simple scatter plotting.

--- src/plotting.py
+++ src/plotting.py
@@ -32,3 +32,34 @@ def readCSV(fname, header=None, drop=False, usecols=None):
     if drop:
         df.dropna(how="all", inplace=True)
     return df
+
+
+def plotSimpleScatter(x, y, data, xlim=None, ylim=None, save=False, path=None):
+    """
+
+    :param x: x-variable
+    :param y: y-variable
+    :param data: Data as Pandas DF
+    :param xlim: X-axis range
+    :param ylim: Y-axis range
+    :param save: Boolean for saving the plot
+    :param path: Path to save the plot
+    :return:
+    """
+
+    sns.set(color_codes=True)
+    sns.set(font_scale=2.5)
+    plt.figure(figsize=(11, 10))
+
+    if xlim is not None:
+        plt.xlim(xmin=xlim[0])
+        plt.xlim(xmax=xlim[1])
+    if ylim is not None:
+        plt.xlim(xmin=ylim[0])
+        plt.xlim(xmax=ylim[1])
+
+    sns.regplot(x=x, y=y, data=data, scatter_kws={"s": 100})
+    plt.show(0)
+
+    if save:
+        plt.savefig(path, format='png')

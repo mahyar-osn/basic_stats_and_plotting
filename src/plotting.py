42a1c39c345bb7be7582df42b5cf818c63387d0c
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2019-02-18 11:57:35 +1300

    Method to read a csv file using Pandas.

--- src/plotting.py
+++ src/plotting.py
@@ -18,3 +18,17 @@ sns.set()
 sns.set_style("whitegrid", {'axes.grid' : False})
 
 
+def readCSV(fname, header=None, drop=False, usecols=None):
+    """
+    Reads a csv file into a Pandas Data Frame.
+
+    :param fname: Filename (including its path).
+    :return: Pandas DF
+    """
+
+    filename = fname
+    df = pd.read_csv(filename, sep=',', header=header, usecols=usecols)
+
+    if drop:
+        df.dropna(how="all", inplace=True)
+    return df
